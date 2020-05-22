2c0bcffd43780c2e1bab806aea0dcaab583651fe
Luis Gonzalez <lgonzalez.us1@gmail.com>
2020-05-22 07:59:57 -0400

    commit with corrections

--- ReferenceBook.java
+++ ReferenceBook.java
@@ -1,8 +1,13 @@
-public class ReferenceBook extends Book{
+public class ReferenceBook extends Book implements Loanable{
 
     
     public ReferenceBook(String title, String author, String publisher, int publishYear, String format, int ISBN){
         super(title, author, publisher, publishYear, format, ISBN);
+    }
+
+    @Override
+    public boolean isLoanable() {
+        return true; 
     }
 
     
